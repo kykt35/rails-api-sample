cdf70197d567874f5883a2377c10709420259c2b
kykt35 <kykt35@gmail.com>
2019-05-15 16:20:09 +0900

    Fix deploy setting

--- config/deploy.rb
+++ config/deploy.rb
@@ -64,6 +64,5 @@ set :pty, true
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
